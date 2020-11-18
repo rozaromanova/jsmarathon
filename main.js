6508af233508d53267129fa6f1b5691a339d037b
rozaromanova <roza.romanova@gmail.com>
2020-11-18 19:30:57 +0100

    homework-2

---
+++ main.js
@@ -0,0 +1,61 @@
+const $btn = document.getElementById('btn-kick');
+
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
+}
+
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,  
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+}
+
+$btn.addEventListener('click', function(){
+    console.log('kicked!')
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+})
+
+function init(){
+    console.log('start the game');
+
+    renderHP(character);
+    renderHP(enemy);
+}
+
+function renderHP(person){
+    renderHPLife(person);
+    renderProgressBarHP(person);
+}
+
+function renderHPLife(person){
+    person.elHP.innerText= person.damageHP + ' / ' + person.defaultHP;
+}
+
+function renderProgressBarHP(person){
+    person.elProgressbar.style.width = person.damageHP + '%';
+}
+
+function changeHP(count, person){
+    if(person.damageHP < count){
+        person.damageHP = 0;
+        alert('Бедный ' + person.name + ' проиграл!');
+        $btn.disabled = true;
+    } else {
+        person.damageHP -= count;
+    }
+    
+    renderHP(person);
+}
+
+function random(num){
+    return Math.ceil(Math.random()*num);
+}
+
+init();
